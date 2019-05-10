fe9e3e00bdd7d7a376bc62ff0bdf4ade6c54decb
“用e <sunzhu_918@163.com>
2019-05-10 11:03:56 +0800

    sunzhu-修改实体类

--- src/main/java/com/yijie/manager/client/model/Adcolumn.java
+++ src/main/java/com/yijie/manager/client/model/Adcolumn.java
@@ -19,12 +19,6 @@ public class Adcolumn {
 	
 	//广告标题
 	private String title;
-	
-	//操作人名字
-	private String handle_name;
-	
-	//操作人id
-	private Integer handle_id;
 
 	public Integer getId() {
 		return id;
@@ -58,26 +52,9 @@ public class Adcolumn {
 		this.title = title;
 	}
 
-	public String getHandle_name() {
-		return handle_name;
-	}
-
-	public void setHandle_name(String handle_name) {
-		this.handle_name = handle_name;
-	}
-
-	public Integer getHandle_id() {
-		return handle_id;
-	}
-
-	public void setHandle_id(Integer handle_id) {
-		this.handle_id = handle_id;
-	}
-
 	@Override
 	public String toString() {
-		return "Adcolumn [id=" + id + ", uuid=" + uuid + ", imgurl=" + imgurl + ", title=" + title + ", handle_name="
-				+ handle_name + ", handle_id=" + handle_id + "]";
+		return "Adcolumn [id=" + id + ", uuid=" + uuid + ", imgurl=" + imgurl + ", title=" + title + "]";
 	}
 
 	public Adcolumn() {
@@ -85,14 +62,14 @@ public class Adcolumn {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Adcolumn(Integer id, String uuid, String imgurl, String title, String handle_name, Integer handle_id) {
+	public Adcolumn(Integer id, String uuid, String imgurl, String title) {
 		super();
 		this.id = id;
 		this.uuid = uuid;
 		this.imgurl = imgurl;
 		this.title = title;
-		this.handle_name = handle_name;
-		this.handle_id = handle_id;
 	}
+	
+	
 
 }
--- src/main/java/com/yijie/manager/client/model/ScoreRule.java
+++ src/main/java/com/yijie/manager/client/model/ScoreRule.java
@@ -1,15 +1,27 @@
 package com.yijie.manager.client.model;
 
 public class ScoreRule {
+	// 主键id
 	private Integer id;
-    private String uuid;
-    //最大预算
-    private double  max_budget;
-    //最小预算
-    private double   min_budget;
-    //积分预算
-    private Integer score_budget;
-    
+
+	// 积分规则uuid
+	private String uuid;
+
+	// 最大预算
+	private double max_budget;
+
+	// 最小预算
+	private double min_budget;
+
+	// 积分预算
+	private Integer score_budget;
+	
+	//状态值 0禁用 1启用
+	private Integer status;
+
+	// 起始条数
+	private Integer begin;
+	
 	//操作人名字
 	private String handle_name;
 	
@@ -56,6 +68,22 @@ public class ScoreRule {
 		this.score_budget = score_budget;
 	}
 
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getBegin() {
+		return begin;
+	}
+
+	public void setBegin(Integer begin) {
+		this.begin = begin;
+	}
+
 	public String getHandle_name() {
 		return handle_name;
 	}
@@ -75,7 +103,8 @@ public class ScoreRule {
 	@Override
 	public String toString() {
 		return "ScoreRule [id=" + id + ", uuid=" + uuid + ", max_budget=" + max_budget + ", min_budget=" + min_budget
-				+ ", score_budget=" + score_budget + ", handle_name=" + handle_name + ", handle_id=" + handle_id + "]";
+				+ ", score_budget=" + score_budget + ", status=" + status + ", begin=" + begin + ", handle_name="
+				+ handle_name + ", handle_id=" + handle_id + "]";
 	}
 
 	public ScoreRule() {
@@ -84,16 +113,18 @@ public class ScoreRule {
 	}
 
 	public ScoreRule(Integer id, String uuid, double max_budget, double min_budget, Integer score_budget,
-			String handle_name, Integer handle_id) {
+			Integer status, Integer begin, String handle_name, Integer handle_id) {
 		super();
 		this.id = id;
 		this.uuid = uuid;
 		this.max_budget = max_budget;
 		this.min_budget = min_budget;
 		this.score_budget = score_budget;
+		this.status = status;
+		this.begin = begin;
 		this.handle_name = handle_name;
 		this.handle_id = handle_id;
 	}
-	
+
 	
 }
